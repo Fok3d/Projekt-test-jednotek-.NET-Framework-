7e2c43aabf1d6b0c9d5be4a31700fd80bd3815f5
Fok3d <sklenarm.06@spst.eu>
2025-03-27 18:24:44 +0100

    pici jebem to je to picovina
    dasddsafdsa

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -7,13 +7,12 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
-
         HerniPostava hrac = new HerniPostava();
         HerniPostava npc = new HerniPostava();
+
         [TestMethod]
         public void TestJmeno_PriDelceNez10Znaku_VyvolaUpozorneni()
         {
-
             hrac.Jmeno = "TotoJeVelmiDlouheJmeno";
             Assert.AreEqual("TotoJeVelmiDlouheJmeno", hrac.Jmeno);
         }
@@ -21,7 +20,6 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestInicializaceLevelu()
         {
-
             Assert.AreEqual(1, hrac.Level);
         }
 
@@ -35,7 +33,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestPoziceX()
         {
-            hrac.X = (2);
+            hrac.X = 2;
             Assert.IsTrue(true);
         }
 
@@ -45,8 +43,6 @@ namespace UnitTestProject1
             hrac.Y = 0;
             Assert.IsTrue(true);
         }
-
-
 
         [TestMethod]
         public void TestPridaniXP()
@@ -88,5 +84,96 @@ namespace UnitTestProject1
             Assert.IsTrue(result.Contains("Je Boss: True"));
         }
 
+        // Přidané testy:
+
+        [TestMethod]
+        public void TestJmeno_NeniNull()
+        {
+            hrac.Jmeno = null;
+            Assert.IsNotNull(hrac.Jmeno);
+        }
+
+        [TestMethod]
+        public void TestSpecializace_Validni()
+        {
+            hrac.Specializace = "Inženýr";
+            Assert.AreEqual("Inženýr", hrac.Specializace);
+        }
+
+        [TestMethod]
+        public void TestSpecializace_Nevalidni()
+        {
+            hrac.Specializace = "Rytíř";
+            Assert.AreNotEqual("Rytíř", hrac.Specializace);
+        }
+
+        [TestMethod]
+        public void TestNPC_BossStatus()
+        {
+            npc.JeBoss = true;
+            Assert.IsTrue(npc.JeBoss);
+        }
+
+        [TestMethod]
+        public void TestNPC_NemuzeZmenitPozici()
+        {
+            npc.ZmenaPozice(10, 15);
+            Assert.AreEqual(0, npc.X);
+            Assert.AreEqual(0, npc.Y);
+        }
+
+        [TestMethod]
+        public void TestXP_NegativniHodnota()
+        {
+            hrac.PridatXP(-50);
+            Assert.AreEqual(0, hrac.XP);
+        }
+
+        [TestMethod]
+        public void TestXP_NulovaHodnota()
+        {
+            hrac.PridatXP(0);
+            Assert.AreEqual(0, hrac.XP);
+        }
+
+        [TestMethod]
+        public void TestXP_PresnyLevelUp()
+        {
+            hrac.PridatXP(100);
+            Assert.AreEqual(2, hrac.Level);
+        }
+
+        [TestMethod]
+        public void TestXP_ViceNasobnyLevelUp()
+        {
+            hrac.PridatXP(250);
+            Assert.AreEqual(3, hrac.Level);
+            Assert.AreEqual(50, hrac.XP);
+        }
+
+        [TestMethod]
+        public void TestZmenaPozice_Hrac()
+        {
+            hrac.ZmenaPozice(3, 7);
+            Assert.AreEqual(3, hrac.X);
+            Assert.AreEqual(7, hrac.Y);
+        }
+
+        
+
+        [TestMethod]
+        public void TestJmeno_NesmíBýtPrazdne()
+        {
+            hrac.Jmeno = "";
+            Assert.AreNotEqual("", hrac.Jmeno);
+        }
+
+        [TestMethod]
+        public void TestXP_NestaciProLevelUp()
+        {
+            hrac.PridatXP(99);
+            Assert.AreEqual(1, hrac.Level); 
+            Assert.AreEqual(99, hrac.XP);
+        }
     }
 }
